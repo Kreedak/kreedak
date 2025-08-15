060615f2c8a7b5dc68a6f782f629fcdcbf9b2bd2
Kreedak <kreedak2009@gmail.com>
2025-08-15 23:08:14 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll", function() {
   const header = document.querySelector("header");
-  if (window.scrollY > 50) {
+  if (window.scrollY > 150) {
     header.classList.add("shrink");
   } else {
     header.classList.remove("shrink");
