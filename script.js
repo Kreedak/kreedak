411d0a003f4c64a9d9fb6fd43b84da524345804c
Kreedak <kreedak2009@gmail.com>
2025-08-15 16:31:55 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-//const header = document.querySelector('header');
+const header = document.querySelector('header');
 
-//window.addEventListener('scroll', () => {
-  //if (window.scrollY > 50) {      // если прокрутили больше 50px
-    //header.style.height = '20px';  // уменьшаем высоту
-  //} else {
-    //header.style.height = '80px';  // возвращаем исходную высоту
-  //}
-//});
-console.log("JS работает!");
-
-document.addEventListener("DOMContentLoaded", () => {
-  alert("Сайт загрузился, JS подключен!");
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {      // если прокрутили больше 50px
+    header.style.height = '20px';  // уменьшаем высоту
+  } else {
+    header.style.height = '80px';  // возвращаем исходную высоту
+  }
 });
