ae699c7bf222bbabcdfe8e28583fd5ae12dfb3bd
Kreedak <kreedak2009@gmail.com>
2025-08-15 17:29:06 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-const header = document.querySelector('header');
-
-function updateHeader(){
-  if (window.scrollY > 50){
-    header.classList.add('shrink');
+window.addEventListener("scroll", function() {
+  const header = document.querySelector("header");
+  if (window.scrollY > 50) {
+    header.classList.add("shrink");
   } else {
-    header.classList.remove('shrink');
+    header.classList.remove("shrink");
   }
-}
-window.addEventListener('scroll', updateHeader);
-window.addEventListener('load', updateHeader);
+});
