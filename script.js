8393d823066d816bbb7068f691edc4269738db6f
Kreedak <kreedak2009@gmail.com>
2025-08-15 16:12:14 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const header = document.querySelector('header');
 
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {      // если прокрутили больше 50px
+    header.style.height = '20px';  // уменьшаем высоту
+  } else {
+    header.style.height = '80px';  // возвращаем исходную высоту
+  }
+});
