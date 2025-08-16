c6c4b3ef5d282498d71dc2ba5234366ce08f5e3d
Kreedak <kreedak2009@gmail.com>
2025-08-16 16:39:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+window.addEventListener("scroll", function() {
+  const header = document.querySelector("header");
+  if (window.scrollY > 150) {
+    header.classList.add("shrink");
+  } 
+  else {
+    header.classList.remove("shrink");
+  }
+});
+
+window.addEventListener("scroll", function() {
+  let nav = document.querySelector("nav");
+  //if (window.innerWidth < 600)
+    if (window.scrollY > 150) {
+        nav.style.gap = "0px";
+    } 
+    else {
+        nav.style.gap = "10px"; 
+    }
+  //else {
+    if (window.scrollY > 150) {
+        nav.classList.add("scrolled");
+    } 
+    else {
+        nav.classList.remove("scrolled"); 
+    }
+  });
